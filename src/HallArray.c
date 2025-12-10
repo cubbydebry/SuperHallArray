629e7e5315ab29b198793e7de4840221f6045c21
Cubby DeBry <cubbydebry@gmail.com>
2025-12-10 16:47:03 -0700

    Rough sensor read functions

---
+++ src/HallArray.c
@@ -0,0 +1,94 @@
+// HallArray.c 
+// Cubby DeBry 2025
+
+#include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+
+#define BUFFER_SIZE 16
+
+#define SPI_PORT spi0
+#define PIN_MISO 0
+#define PIN_SCK 2
+#define PIN_MOSI 3
+
+#define CS1 4
+#define CS2 5
+#define CS3 6
+#define CS4 7
+#define CS5 8
+
+#define IN0 (0x7 << 3)
+#define IN1 (0x6 << 3)
+#define IN2 (0x5 << 3)
+#define IN3 (0x0 << 3)
+#define IN4 (0x1 << 3)
+#define IN5 (0x2 << 3)
+#define IN6 (0x3 << 3)
+#define IN7 (0x4 << 3)
+
+static uint adc_buffer[BUFFER_SIZE];
+
+void spi_init_adc_bus(void){
+    spi_init(SPI_PORT, .1 * 1000 * 1000); // 1 MHz
+    spi_set_format(SPI_PORT, 8, SPI_CPOL_0, SPI_CPHA_1, SPI_MSB_FIRST);
+
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+
+    uint cs_pins[] = {CS1, CS2, CS3, CS4, CS5};
+    for (int i = 0; i < 5; i++){
+        gpio_init(cs_pins[i]);
+        gpio_set_dir(cs_pins[i], GPIO_OUT);
+        gpio_put(cs_pins[i], 1); // Inactive (Active Low)
+    }
+}
+
+void adc_init(void){
+
+}
+
+void adc_read(void){
+
+}
+
+uint16_t spi_transfer16(uint16_t tx){
+    uint8_t tx_buf[2] = { (tx >> 8) & 0xFF, tx & 0xFF};
+    uint8_t rx_buf[2];
+    spi_write_read_blocking(SPI_PORT, tx_buf, rx_buf, 2);
+    return ((uint16_t)rx_buf[0] << 8) | rx_buf[1];
+}
+
+void main_task(__unused void *params) {
+    printf("Entering main task\n");
+    while(1) {
+        gpio_put(CS1, 0);
+        spi_transfer16((uint16_t)IN0 << 8);
+        uint16_t raw = spi_transfer16((uint16_t)IN0 << 8);
+        gpio_put(CS1, 1);
+        printf("ADC Reading: %u\n", raw);
+        // vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+int main(void)
+{
+    stdio_init_all();
+    cyw43_arch_init();
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    spi_init_adc_bus();
+    TaskHandle_t readtask;
+    xTaskCreate(main_task, "Thread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, &readtask);
+    printf("Starting scheduler\n");
+    vTaskStartScheduler();
+    return 0;
+}
